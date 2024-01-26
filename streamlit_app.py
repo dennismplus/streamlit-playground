fe62c1ae258694cb16591bc9c434d1b8835c1ff9
dennis <simbataisa@gmail.com>
2024-01-26 23:17:15 +0800

    neon

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from sqlalchemy import text
 from streamlit_echarts import st_echarts
 
 # Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.connection('local_db_external', type='sql')
+conn = st.connection('neon_db', type='sql')
 
 # Yahoo Finance
 # Input for the stock ticker
